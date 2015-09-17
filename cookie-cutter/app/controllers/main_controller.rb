692e91dedce0a1a120e3dfc088100d18dfeaad38
wakarana1 <wu.chris@outlook.com>
2015-09-17 16:04:19 -0700

    added yelp api keys

--- cookie-cutter/app/controllers/main_controller.rb
+++ cookie-cutter/app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
 
   def index
+    @response = HTTParty.get"http://api.yelp.com/v2/search/?term=cookies&location=Los Angeles, CA&=Food&radius_filter=15000"
   end
 end
