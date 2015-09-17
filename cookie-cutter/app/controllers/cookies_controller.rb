e28bf8e174808c68409c3d2fe721de882c5ac044
wakarana1 <wu.chris@outlook.com>
2015-09-17 16:10:41 -0700

    created cookies controller

---
+++ cookie-cutter/app/controllers/cookies_controller.rb
@@ -0,0 +1,6 @@
+class CookiesController < ApplicationController
+
+    def index
+        @response = HTTParty.get"http://api.yelp.com/v2/search/?term=cookies&location=Los Angeles, CA&=Food&radius_filter=15000"
+    end
+end
--- cookie-cutter/app/controllers/main_controller.rb
+++ cookie-cutter/app/controllers/main_controller.rb
@@ -1,6 +1,3 @@
 class MainController < ApplicationController
-
-  def index
-    @response = HTTParty.get"http://api.yelp.com/v2/search/?term=cookies&location=Los Angeles, CA&=Food&radius_filter=15000"
-  end
+    
 end
